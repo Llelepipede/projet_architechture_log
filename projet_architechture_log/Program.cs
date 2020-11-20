1e3b042c5a04972ab020820419bfd61fc22fe79e
Llelepipede <pchesneau3103@gmail.com>
2020-11-20 09:51:06 +0100

    new commit

---
+++ projet_architechture_log/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace projet_architechture_log
+{
+    class Program
+    {
+        static void Main()
+        {
+            traducteur traducteur = new traducteur(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\srt\test.srt");
+            Task.WaitAll(traducteur.affichage());
+
+        }
+    }
+}
---
+++ projet_architechture_log/Soustitre.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet_architechture_log
+{
+    class Soustitre
+    {
+
+        public TimeSpan heuredep;
+        public TimeSpan heurefin;
+        public string value;
+
+        public Soustitre(TimeSpan heuredep, TimeSpan heurefin, string value)
+        {
+            this.heuredep = heuredep;
+            this.heurefin = heurefin;
+            this.value = value;
+        }
+    }
+}
