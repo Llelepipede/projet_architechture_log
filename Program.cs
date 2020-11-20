bee17b585187c6b4fadb2950bd10542e21a3e1b1
Llelepipede <pchesneau3103@gmail.com>
2020-11-20 09:13:37 +0100

    first and last commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace projet_log
+{
+    class Program
+    {
+        static void Main()
+        {
+            traducteur traducteur = new traducteur(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\srt\test.srt");
+            Task.WaitAll(traducteur.affichage());
+            
+        }
+    }
+}
